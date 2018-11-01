1965c997b32e8a4b5ae0ec664e60a08de04b88fa
Jamie <jamiemckeown469@gmail.com>
2018-11-01 08:46:48 -0400

    1/11/2018

--- jfeo.js
+++ jfeo.js
@@ -9,7 +9,7 @@ $(".card-body2").on("click", function (){
 });
 
 $(".card-body3").on("click", function(){
-    alert("Seulement disponible en mai 2019");
+    alert("Disponible avril/mai 2019");
 });
 
 //  click on dmsa logo and get re-directed
@@ -20,5 +20,6 @@ $(".dmsa").on("click",function(){
 
 
 // Weather Widget
+
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
