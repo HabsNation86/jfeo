5c2fb4e96931c805eb49466828e25eb12d66c667
Jamie <jamiemckeown469@gmail.com>
2018-11-10 09:21:59 -0500

    eng to fre on click

--- jfeo.js
+++ jfeo.js
@@ -13,42 +13,74 @@ $(".card-body3").on("click", function(){
 });
 
 $(".card-body4").on("click", function(){
-    window.location = "https://www.meteomedia.com/ca/meteo/quebec/deux-montagnes";
+    window.location = "https://www.theweathernetwork.com/ca/weather/quebec/deux-montagnes";
 })
 
 //  click on dmsa logo and get re-directed
 
 $(".dmsa").on("click",function(){
-    window.location = "http://www.dmsa.ca/fr/index.html";
+    window.location = "http://www.dmsa.ca/en/index.html";
 });
 
 
 // converting english to french on button click
 
-$('#btn').on("click", function () {
+$('#btn1').on("click", function () {
     $('#card-title1').html('Classements')
 });
 
-$('#btn').on("click", function () {
+$('#btn1').on("click", function () {
     $('#card-title2').html('Résultats')
 });
 
-$('#btn').on("click", function () {
+$('#btn1').on("click", function () {
     $('#card-title3').html('Horaires')
 });
 
-$('#btn').on("click", function () {
+$('#btn1').on("click", function () {
     $('#card-title4').html('Météo')
 });
 
-$('#btn').on("click", function () {
+$('#btn1').on("click", function () {
     $('#card-text4').html('7 jours')
 });
 
-$('#btn').on("click", function () {
-    $('#title1').html('Ligue Mark')
+$('#btn1').on("click", function () {
+    $('#headerId').html('Ligue Mark')
 });
 
-$('#btn').on("click", function () {
-    $('#display-4').html('Ligue Mark')
+$('#btn1').on("click", function () {
+    $('.display-4').html('Ligue Mark 2019')
+});
+
+
+// Converting French to English
+
+
+$('#btn2').on("click", function () {
+    $('#card-title1').html('Standings')
+});
+
+$('#btn2').on("click", function () {
+    $('#card-title2').html('Results')
+});
+
+$('#btn2').on("click", function () {
+    $('#card-title3').html('Schedules')
+});
+
+$('#btn2').on("click", function () {
+    $('#card-title4').html('Weather')
+});
+
+$('#btn2').on("click", function () {
+    $('#card-text4').html('7 days')
+});
+
+$('#btn2').on("click", function () {
+    $('#headerId').html("Mark's League")
+});
+
+$('#btn2').on("click", function () {
+    $('.display-4').html("Mark's League 2019")
 });
