2e8e52b08ca13363e2ed79fdc8f460c62df845a1
Jamie <jamiemckeown469@gmail.com>
2018-11-09 19:15:31 -0500

    eng to fre on click

--- jfeo.js
+++ jfeo.js
@@ -28,3 +28,16 @@ $(".dmsa").on("click",function(){
 
 !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
 
+
+// converting english to french on button click
+
+
+    document.getElementById('btn').onclick = function () {
+        document.getElementById('card-title1').innerHTML = 'Classements';
+        document.getElementById('card-title2').innerHTML = 'Résultats';
+        document.getElementById('card-title3').innerHTML = 'Horaires';
+        document.getElementById('card-title4').innerHTML = 'Météo';
+        document.getElementById('card-text4').innerHTML = '7 jours';
+        document.getElementById('title1').innerHTML = 'Ligue Mark';
+        document.getElementById('display-4').innerHTML = 'Ligue Mark';
+}
